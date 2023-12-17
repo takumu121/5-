689a8931f79bb3967a14963e5819b5ec2d35f91a
takumu matsuura <takumushome@takumus-MacBook-Air.local>
2023-12-17 15:16:32 +0900

    DrawText

--- Src/main.js
+++ Src/main.js
@@ -1,27 +1,5 @@
 async function main() {
-    await Sleep(1000)
-
-    DrawText("Hello World!", 100, 100)
-
-    await Sleep(1000)
-    
-    SetColor("red")
-    SetFont("50px serif")
-    DrawText("Let's Coding!", 100, 200)
-
-    await Sleep(1000)
-
-    
-    const chick = (await LoadImages(["/Assets/twimoji_chick.png"]))[0]
-    
-    while (true){
-        //clear
-        SetColor("white")
-        DrawRect(0, 0, GetCanvasSize()[0], GetCanvasSize()[1])
-
-        DrawImage(chick, GetMouseX(), GetMouseY(), 100, 100)
-    
-        //cool time
-        await Sleep(1000/16)
+    //コメントはつけるほどえらい
+    DrawText("anongagnipgeag",300,100)
+    DrawText(241975,300,599)
     }
-}
