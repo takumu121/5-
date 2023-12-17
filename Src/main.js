1a18a346dcd308d343c19a9372c1d7e8ae50eda5
takumu matsuura <takumushome@takumus-MacBook-Air.local>
2023-12-17 15:45:40 +0900

    待て

--- Src/main.js
+++ Src/main.js
@@ -1,21 +1,35 @@
 async function main() {
     //コメントはつけるほどえらい
-    DrawText("anongagnipgeag",300,100)
-    SetColor("#agit 0f8ff")
-    DrawText(241975,300,599)
+    // DrawText("anongagnipgeag",300,100)
+    // SetColor("#agit 0f8ff")
+    // DrawText(241975,300,599)
 
-    DrawText(1 + 1,300,300)
-    DrawText(1 / 2,300,300)
-    DrawText(5 % 4,300,300)
+    // DrawText(1 + 1,300,300)
+    // DrawText(1 / 2,300,300)
+    // DrawText(5 % 4,300,300)
 
-    //変数
-    let a = 1 
-    DrawText (a ,300,400)
+    // //変数
+    // let a = 1 
+    // DrawText (a ,300,400)
 
-    a = 2
-    DrawText (a ,300,400)
+    // a = 2
+    // DrawText (a ,300,400)
 
-    a -= 1
-    DrawText (a ,300,400)
+    // a -= 1
+    // DrawText (a ,300,400)
+
+    // //ループ
+    // for(let cnt = 0; cnt<3 * 100;cnt++){
+    //     DrawText(cnt, 300, 300 + cnt * 100)
+    // }
+
+    // //無限ループ
+    // for(let cnt = 0; ; cnt++){
+    //     DrawText("無限ループ",300,300)
+    // }
     
+    //待て
+    DrawText("まte",300,300)
+    await Sleep(1000)
+    DrawText("まった",200,300)
     }
